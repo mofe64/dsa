49caf31121b49c04909a0f244c7cdd91d28913c9
Eyimofe Ogunbiyi <ogunbiyioladapo33@gmail.com>
2022-09-19 16:13:20 +0100

    shell sort impl

---
+++ src/com/nubari/elementarysorts/ShellSort.java
@@ -0,0 +1,23 @@
+package com.nubari.elementarysorts;
+
+public class ShellSort extends Sort {
+    @Override
+    public void sort(Comparable[] a) {
+        int N = a.length;
+        int h = 1;
+        while (h < N / 3) h = 3 * h + 1;
+        while (h >= 1) { // h-sort the array.
+            for (int i = h; i < N; i++) {
+                for (int j = i; j >= h; j -= h) {
+                    if (less(a[j], a[j - h])) {
+                        exch(a, j, j - h);
+                    } else {
+                        break;
+                    }
+                }
+            }
+            h = h / 3;
+
+        }
+    }
+}
