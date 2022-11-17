01066db118c42398562060dfcf1f8c9e67ec46f9
Eyimofe Ogunbiyi <ogunbiyioladapo33@gmail.com>
2022-11-17 11:01:40 +0000

    updated shellsort

--- src/com/nubari/sorts/ShellSort.java
+++ src/com/nubari/sorts/ShellSort.java
@@ -1,16 +1,18 @@
 package com.nubari.sorts;
 
-public class ShellSort extends Sort {
-    @Override
-    public void sort(Comparable[] a) {
+public class ShellSort {
+
+    public void sort(int[] a) {
         int N = a.length;
         int h = 1;
         while (h < N / 3) h = 3 * h + 1;
         while (h >= 1) { // h-sort the array.
             for (int i = h; i < N; i++) {
                 for (int j = i; j >= h; j -= h) {
-                    if (less(a[j], a[j - h])) {
-                        exch(a, j, j - h);
+                    if (a[j] < a[j - h]) {
+                        int temp = a[j - h];
+                        a[j - h] = a[j];
+                        a[j] = temp;
                     } else {
                         break;
                     }
