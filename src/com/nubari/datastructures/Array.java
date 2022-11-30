7c42c9877b04adc091ade673187a4a062a108261
Eyimofe Ogunbiyi <ogunbiyioladapo33@gmail.com>
2022-11-30 18:36:16 +0000

    dynamic array impl

--- src/com/nubari/datastructures/Array.java
+++ src/com/nubari/datastructures/Array.java
@@ -1,10 +1,12 @@
 package com.nubari.datastructures;
 
+import java.util.Iterator;
+
 @SuppressWarnings("unchecked")
 public class Array<T> implements Iterable<T> {
     private T[] arr;
-    private int len = 0;
-    private int capacity = 0;
+    private int len = 0; // user length
+    private int capacity = 0; // internal length
 
     public Array() {
         this(16);
@@ -49,15 +51,84 @@ public class Array<T> implements Iterable<T> {
                 capacity = capacity * 2;
             }
         }
-        T[] new_arr = (T[]) new Object[capacity];
+        T[] newArr = (T[]) new Object[capacity];
         for (int i = 0; i < len; i++) {
-            new_arr[i] = arr[i];
+            newArr[i] = arr[i];
         }
-        arr = new_arr;
+        arr = newArr;
         arr[len++] = elem;
     }
 
-    public T removeAt(int rm_index) {
+    public T removeAt(int rmIndex) {
+        if (rmIndex >= len && rmIndex < 0) {
+            throw new IndexOutOfBoundsException();
+        }
+        T data = arr[rmIndex];
+        T[] newArr = (T[]) new Object[len - 1];
+        for (int i = 0, j = 0; i < len; i++, j++) {
+            if (i == rmIndex) {
+                j--;
+            } else {
+                newArr[j] = arr[i];
+            }
+        }
+        arr = newArr;
+        capacity = --len;
+        return data;
+    }
+
+    public boolean remove(Object obj) {
+        for (int i = 0; i < len; i++) {
+            if (arr[i].equals(obj)) {
+                removeAt(i);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int indexOf(Object obj) {
+        for (int i = 0; i < len; i++) {
+            if (arr[i].equals(obj)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public boolean contains(Object obj) {
+        return indexOf(obj) != -1;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+            int index = 0;
+
+            @Override
+            public boolean hasNext() {
+                return index < len;
+            }
+
+            @Override
+            public T next() {
+                return arr[index++];
+            }
+        };
+    }
+
+    @Override
+    public String toString() {
+        if (len == 0) {
+            return "[]";
+        }
+        StringBuilder sb = new StringBuilder(len).append("[");
+        for (int i = 0; i < len - 1; i++) {
+            sb.append(arr[i]).append(", ");
+        }
+        return sb.append(arr[len - 1]).append("]").toString();
 
     }
+
+
 }
